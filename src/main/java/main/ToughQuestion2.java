8e40379b0361e44bcda8c504e86100b16cf90ef6
saikiran loka <lokasaikiran28@gmail.com>
2019-08-08 10:22:43 +0530

    toughquestion2 successful

--- src/main/java/main/ToughQuestion2.java
+++ src/main/java/main/ToughQuestion2.java
@@ -19,15 +19,15 @@ class ToughQuestion2 {
                         {
                         case 1 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
-                        	//call the function which calculate the calories for only bread
-                        	System.out.println();
+                        	System.out.printf("%.3f",cal.calculateEnergy(cal.calculateCalories(bread)));//call the function which calculate the calories for only bread
+                        	System.out.println(" kJ of energy generated from " + cal.returnCalories()+" calories");
                         	break;
                         case 2 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
                         	System.out.println("Enter the number teaspoon of Jam");
                         	jam = Integer.parseInt(br.readLine());
-                        	//call the function which calculate the calories for only bread & jam
-                        	System.out.println();
+                        	System.out.printf("%.3f",cal.calculateEnergy(cal.calculateCalories(bread, jam)));//call the function which calculate the calories for only bread & jam
+                        	System.out.println(" kJ of energy generated from " + cal.returnCalories()+" calories");
                         	break;
                         case 3 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
@@ -36,7 +36,8 @@ class ToughQuestion2 {
                         	System.out.println("Enter the number teaspoon of Butter");
                         	butter = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for  bread,jam & butter
-                        	System.out.println();
+                        	System.out.printf("%.3f",cal.calculateEnergy(cal.calculateCalories(bread, jam, butter)));
+                        	System.out.println(" kJ of energy generated from " + cal.returnCalories()+" calories");
                         	break;
                         }
                         //System.out.printf("%.3f",ca.calculateEnergy());
@@ -45,10 +46,30 @@ class ToughQuestion2 {
 }
 class Calories {
             
-            int calculateCalories() {
-            //you have to overload this function for three parameters refer the question
-            	return -1;
-            }
+	private int calories;
+    public int calculateCalories(int nBread) {
+    	calories = 74 * nBread;
+    	return calories;
+    }
+    
+    public int calculateCalories(int nBread, int nJam) {
+        	calories = 74 * nBread + 26 * nJam;
+        	return calories;
+        }
+    public int calculateCalories(int nBread, int nJam, int nButter) {
+        //you have to overload this function for three parameters refer the question
+        	calories = 74 * nBread + 26 * nJam + 102 * nButter;
+        	return calories;
+        }
+    public double calculateEnergy(int calorie) {
+    	// Given 1Kcal = 4.1868 KJ
+    	// So, 1 calorie = 4.168 J
+    	// So, n calorie = n * 4.186 J = n * 4.186 / 1000 KJ
+    	return calorie * 4.1868 / 1000;
+    }
+    public int returnCalories() {
+    	return calories;
+    }
             
             
             
